b0ab8a4bd08bd90e6c7aa04a50ad2e665b826b43
Abrar33 <barumalik333@gmail.com>
2022-08-06 19:18:05 -0700

    initial commit

---
+++ ifElse.js
@@ -0,0 +1,24 @@
+// let age= 34;
+// if(age=>19){
+   
+//     console.log("you are played for senior");
+
+// }
+// else{
+//     console.log("wrong statement")
+// }
+let age=10;
+if(age<=10)
+{
+console.log("You need to focus on future");
+}
+else if(age>10){
+console.log("YOU NEED TO STUDY");
+}
+else if(age==12){
+    console.log("helioo welcome home");
+}
+else
+{
+    console.log("wrong statement")
+};
---
+++ practice2.js
@@ -0,0 +1,71 @@
+// function plus(a,b){
+//     return a+b;
+// }
+// alert(plus(10,10));
+// prompt("What is your name?","Name:","name:");
+// console.log(prompt("What is your name?","Name:","name:"));
+// console.log(confirm("Continue learning JS?"));
+// console.error("this is an error");
+// console.warn("this is an warning");
+// // alertabc("it is error");
+// console.log('%c hey there i am logged to console',
+// "color:lime; font-size:20px; font-weight:bold" )
+// alert("js is cool")
+//      ****----->VARIABLES<-------****
+// let favFood="piza";
+// let favColor="Red";
+// let favDrink="pure water";
+// console.log(favDrink);
+// favFood="Mango"
+// console.log(favFood)
+// function myBirthday(){
+//      date=10;
+//     month=4;
+    
+//     console.log(date,month)
+// }
+// myBirthday()
+let a,b,c,d,e,f;
+a=10;
+b=2;
+c=3;
+d=4;
+e=33;
+f=a+b+c+d+e;
+// document.write(f);
+// document.write("<br>");
+// document.write(typeof f);
+// let array={
+//     name:"Abrar",
+//     age:22,
+//     profession:"coding",
+// }
+// // console.log(array.age);
+// let funKi=function()
+// {
+// console.log(array.age,":",f);
+// //console.log(array);
+// }
+// funKi()
+function newOp(){
+    let courses={
+        lectures:30,
+        setions:10,
+        Language:"javaScript",
+        topic:"object"
+    }
+    let lec=["...courses"]["new"]({
+        name:21,
+    } )
+    console.log(lec)
+    // function enroll() {
+    //     if (lec>29){                        
+    //         console.log("you need to work on it more than 30 days")
+    //     }
+    //     else{
+    //         console.log("wrong statement")
+    //     }
+    // }
+    // enroll()
+} 
+    newOp()
